2684791828fdd86ae03aa3957f8b1e1aab6f14b1
EmilJohan <emil.molberg@hotmail.no>
2024-08-07 08:34:23 +0200

    path fix

--- Smelt.py
+++ Smelt.py
@@ -577,10 +577,11 @@ class Smelt(QWidget):
             commands (list): A list of command attribute names to execute.
         """
         for i, cmd in enumerate(commands):
-            log_path = os.path.join(self.output_folder, 'logs', '{}_{}_log.txt'.format(self.output_folder_name, commands[i]))
+            log_path = os.path.join(self.output_folder, 'logs', '{}_log.txt'.format(commands[i]))
             if platform.system() == 'Windows':
-                log_path = log_path.replace('\\', '/')
-            os.environ['FFREPORT'] = fr"file={log_path}:level=32"
+                log_path = os.path.normpath(log_path)
+            ffreport_value = "file=" + log_path + ":level=32"
+            os.environ['FFREPORT'] = ffreport_value
             self.output_text.append(log_path)
             step_text = "Step {}/{}: Running {}".format(i + 1, len(commands), cmd.replace('_', ' ').title())
             self.step_label.setText(step_text)
